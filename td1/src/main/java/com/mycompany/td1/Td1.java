45d2942d62301b12dcefea9df9940ef00549d35d
Ndiamelayegueye <gueyendiamelaye@gmail.com>
2024-05-16 14:59:50 +0000

    td

---
+++ td1/src/main/java/com/mycompany/td1/Td1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.td1;
+
+/**
+ *
+ * @author pc
+ */
+public class Td1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
